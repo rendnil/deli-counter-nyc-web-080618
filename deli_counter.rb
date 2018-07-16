1d030552698f5f282b758fa5eea43484348f9027
Andrew Lindner <andrew.lindner.15@gmail.com>
2018-07-16 16:45:04 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+
+
+katz_deli  = []
+
+def line(line_array)
+
+  if line_array.size == 0
+    "The line is currently empty."
+  else
+     
+     detail_line = [ ]
+     line_array.each_with_index do 
+  
+  end
+  
+
+end  
